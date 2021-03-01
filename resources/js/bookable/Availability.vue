33e04d521b68f9c88e04540f4a9ca06596bc3a39
Bilguun Nomch <bbg.1015@gmail.com>
2021-03-01 10:56:11 -0600

    created availability form inputs

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -1,5 +1,52 @@
 <template>
     <div>
-        <h6>Check Availability</h6>
+        <h6 class="text-uppercase text-secondary font-weight-bolder">
+            Check Availability
+        </h6>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="from">From</label>
+                <input
+                    type="text"
+                    class="form-control form-control-sm"
+                    name="from"
+                    placeholder="Start date"
+                    v-model="from"
+                />
+            </div>
+
+            <div class="form-group col-md-6">
+                <label for="to">To</label>
+                <input
+                    type="text"
+                    class="form-control form-control-sm"
+                    name="to"
+                    placeholder="End date"
+                    v-model="to"
+                />
+            </div>
+        </div>
+
+        <button class="btn btn-secondary btn-block">Check!</button>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            from: null,
+            to: null
+        };
+    }
+};
+</script>
+
+<style scoped>
+label {
+    font-size: 0.7rem;
+    text-transform: uppercase;
+    color: gray;
+    font-weight: bolder;
+}
+</style>
